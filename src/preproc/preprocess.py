6120417bb54add1fe7e775aba833d246d7dc4755
Adam Mesha <adam@mesha.org>
2018-02-13 20:24:55 +0200

    Add docstrings

--- src/preproc/preprocess.py
+++ src/preproc/preprocess.py
@@ -4,7 +4,28 @@ from preproc.errors import PreprocessingError
 
 
 class Preprocess(object):
+    """Class to encapsulate the preprocessing pipeline with variable steps.
+
+    A Preprocess object encapsulates the preprocessing pipeline.  Steps
+    should inherit from preproc.steps.Step, and can either be passed in
+    to the __init__ method, or added later through the add method.  At
+    each step, the output from the previous step is passed as input to
+    the next step.
+
+    A Preprocess object works in two modes, fit and transform.  fit mode
+    is used to set parameters in steps which learn parameters (for
+    example, the average value in the training dataset), and transform
+    mode is used to run new data through the preprocessing pipeline
+    after the parameters have been learned.
+
+    """
+
     def __init__(self, steps=()):
+        """Create a new preprocessing pipeline.
+
+        Initial steps can be passed as an iterable.
+
+        """
         self.steps = list(steps)
         self.fitted = False
 
@@ -16,10 +37,24 @@ class Preprocess(object):
             raise PreprocessingError('Preprocessing object already fitted')
 
     def add(self, step):
+        """Add a step to the end of this preprocessing pipeline.
+
+        Raises: preproc.errors.PreprocessingError if the pipeline has
+        already been fitted.
+
+        """
         self._check_not_fitted()
         self.steps.append(step)
 
     def fit(self, data):
+        """Run with training data, allowing steps to learn their parameters.
+
+        Raises: preproc.errors.PreprocessingError if the pipeline is
+        already fitted.
+
+        Returns: numpy.ndarray of the processed data
+
+        """
         data = np.atleast_2d(data)
         self._check_not_fitted()
         for step in self.steps:
@@ -27,6 +62,14 @@ class Preprocess(object):
         self.fitted = True
 
     def transform(self, data):
+        """Run the pipeline on new data.
+
+        The data will be transformed according to the pipeline's steps
+        and the parameters learned during the fitting stage.
+
+        Returns: numpy.ndarray of the processed data
+
+        """
         self.fitted = True
         data = np.atleast_2d(data)
         for step in self.steps:
--- src/preproc/steps.py
+++ src/preproc/steps.py
@@ -4,6 +4,15 @@ from preproc.errors import PreprocessingError
 
 
 class Step(object):
+    """Base class for defining preprocessing steps.
+
+    A preprocessing step needs to define the method transform, which
+    will apply the step to the given data and return the preprocessed
+    data.  If the preprocessing step needs to learn parameters from
+    training data, it should implement the fit method to learn its
+    parameters from training data.
+
+    """
     def __init__(self):
         pass
 
@@ -18,25 +27,55 @@ class Step(object):
 
 
 class Log(Step):
+    """Step to take the natural logarithm of the data points."""
+
     def transform(self, data):
         return np.log(data)
 
 
 class Diff(Step):
+    """Step to take the difference between successive columns.
+
+    The resulting preprocessed data will have one fewer column than the
+    input.
+
+    """
+
     def transform(self, data):
         return np.diff(data, axis=1)
 
 
 class SelectWavelengths(Step):
+    """Step to select specific columns in the input data.
+
+    The columns selected are controlled by the from_ and to parameters
+    to the __init__ method.  All data points between from_ and to
+    including the endpoints will be returned by the preprocessing step.
+    Indices are 0-based.
+
+    The resulting data will have a different number of columns,
+    depending on the arguments passed to the step.
+
+    """
+
     def __init__(self, from_, to):
         self.from_ = from_
         self.to = to
 
     def transform(self, data):
-        return np.array(data)[:, self.from_:self.to+1]
+        return np.array(data)[:, self.from_ : self.to+1]
 
 
 class SubtractAvg(Step):
+    """Step to calculate average of each column and subtract it from the
+    data to be preprocessed.
+
+    In the fit stage the average of each column in the training data is
+    calculated, and in the transform stage the learned average is
+    subtracted from each column.
+
+    """
+
     def __init__(self):
         self.avg = None
         self.n = 0
@@ -66,4 +105,4 @@ class SubtractAvg(Step):
             self.avg = np.zeros(data.shape[1])
         self.avg += np.sum(data, axis=0)
         self.n += data.shape[0]
-        return data  # XXX
+        return self.transform(data)
